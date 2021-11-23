a41abbc935e96c3658bd1e6ea01427ba52eb2594
Shijh <838239178@qq.com>
2021-11-23 21:59:20 +0800

    9

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/cemu_vue/',
   resolve: {
     alias: {
       'vue': 'vue/dist/vue.esm-bundler.js', // 定义vue的别名，如果使用其他的插件，可能会用到别名
